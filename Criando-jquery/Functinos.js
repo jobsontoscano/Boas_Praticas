235d3623a15bd91fa9e699513a07b4b232f7b4fd
jobsontoscano <jjortb2@cin.ufpe.br>
2017-11-17 18:25:46 -0300

    atividades da semana

---
+++ Criando-jquery/Functinos.js
@@ -0,0 +1,11 @@
+function $(valor){
+	var valor_final = valor;
+	var char = valor.substring(0,1);
+	// if (char == '.') {
+	// 	return document.getElementByClassName(valor_final);
+	// }else if(char == '#'){
+	// 	return document.getElementById(valor_final);
+	// }else{
+	// 	document.getElementsByTagName(valor_final);
+	// }
+}
